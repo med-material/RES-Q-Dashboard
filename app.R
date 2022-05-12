59342985bdeac3e4131b010a3af4d1c45e352ac8
gabCodes <47746090+gabCodes@users.noreply.github.com>
2022-05-12 10:08:17 +0200

    New application and module that helps it create a scatter plot for variable exploration of database.

---
+++ app.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+library(shiny)
+library(plotly)
+library(styler)
+library(lintr)
+library(shinylogs)
+
+source("modules/scatterPlot.R", local = T)
+log_dir <- "logs"
+
+#If you want to have the logs folder prompted on application stop
+#onStop(function() {
+#  browseURL(url = log_dir)
+#})
+
+ui <- navbarPage(
+  "Res Q Dashboard",
+  tabPanel(
+    "Hospital Overview",
+    fluidPage(
+      fluidRow(
+        column(
+          5,
+          h1("Quality Indicators"),
+          tags$hr(),
+          tags$p("First Item in list"),
+          tags$hr(),
+          tags$p("Second Item"),
+          tags$hr(),
+          tags$p("Third Item"),
+          tags$hr(),
+          tags$p("Fourth Item"),
+          tags$hr(),
+          tags$p("Fifth Item"),
+          tags$hr(),
+          tags$p("Sixth Item")
+        ),
+        column(
+          7,
+          h1("Visualization"),
+          tabsetPanel(type = "tabs",
+                      tabPanel("Plot", scatterPlot_UI(id = "module_1", database = mtcars)),
+                      tabPanel("Table", scatterPlot_Table(id = "module_1"))
+          )
+        )
+      )
+    )
+  ),
+  tabPanel("Patient Profiles"),
+  tabPanel("Bleeding"),
+  tabPanel("Imaging"),
+  tabPanel("Treatment"),
+  tabPanel("Phase One"),
+  tabPanel("Discharge")
+)
+
+server <- function(input, output, session) {
+  # Store JSON with logs in the logs dir
+  track_usage(
+    storage_mode = store_json(path = log_dir)
+  )
+  
+  scatterPlot_server(id = "module_1", mtcars)
+}
+
+shinyApp(ui = ui, server = server)
---
+++ modules/scatterPlot.R
@@ -0,0 +1,50 @@
+scatterPlot_UI <- function(id, database) {
+  ns <- NS(id)
+
+  tagList(
+    plotlyOutput(
+      outputId = ns("plot")
+    ),
+    selectInput(
+      inputId = ns("selected_col"),
+      label = "Select column",
+      choices = colnames(database),
+      selected = colnames(database)[1]
+    ),
+    selectInput(
+      inputId = ns("selected_col2"),
+      label = "Select column",
+      choices = colnames(database),
+      selected = colnames(database)[2]
+    ),
+    checkboxInput(
+      inputId = ns("smooth_line"),
+      label = "Add smoothing",
+      value = FALSE
+    )
+  )
+}
+
+scatterPlot_Table <- function(id) {
+  ns <- NS(id)
+  dataTableOutput(
+    outputId = ns("table")
+  )
+}
+
+scatterPlot_server <- function(id, database) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      output$plot <- renderPlotly({
+        plot <- ggplot(database, aes(x = .data[[input$selected_col]], y = .data[[input$selected_col2]])) +
+          geom_point()
+        if (input$smooth_line == TRUE) {
+          plot <- plot + geom_smooth(se = FALSE)
+        }
+        ggplotly(plot)
+      })
+      output$table <- renderDataTable(database %>% select(input$selected_col, input$selected_col2), options = list(pageLength = 5))
+    }
+  )
+}
