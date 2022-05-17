5e429e44cf4a169f171d82541f8d54b6de9a996e
gabCodes <47746090+gabCodes@users.noreply.github.com>
2022-05-17 16:15:32 +0200

    Renamed id's for clarity and added server calls for new server functions.

--- app.R
+++ app.R
@@ -4,9 +4,14 @@ library(plotly)
 library(styler)
 library(lintr)
 library(shinylogs)
+library(DT)
+library(shinydashboard)
+library(sparkline)
+library(htmlwidgets)
 
-source("modules/scatterPlot.R", local = T)
+source("modules/Visualization.R", local = T)
 source("modules/tabPages_Layout.R", local = T)
+source("modules/QI.R", local = T)
 
 #If you want to have the logs folder prompted on application stop
 #onStop(function() {
@@ -17,23 +22,23 @@ ui <- navbarPage(
   "Res Q Dashboard",
   tabPanel(
     "Hospital Overview",
-    tabPages(tab_id = "module_1", tab_df = mtcars)
+    tabPages(tab_id = "tab1", tab_df = mtcars)
   ),
   tabPanel(
     "Patient Profiles",
-    tabPages(tab_id = "module_2", tab_df = iris)
+    tabPages(tab_id = "tab2", tab_df = iris)
   ),
   tabPanel(
     "Bleeding",
-    tabPages(tab_id = "module_3", tab_df = ToothGrowth)
+    tabPages(tab_id = "tab3", tab_df = ToothGrowth)
   ),
   tabPanel(
     "Imaging",
-    tabPages(tab_id = "module_4", tab_df = PlantGrowth)
+    tabPages(tab_id = "tab4", tab_df = PlantGrowth)
   ),
   tabPanel(
     "Treatment",
-    tabPages(tab_id = "module_5", tab_df = USArrests)
+    tabPages(tab_id = "tab5", tab_df = USArrests)
   ),
   tabPanel("Phase One"),
   tabPanel("Discharge")
@@ -45,11 +50,22 @@ server <- function(input, output, session) {
     storage_mode = store_null()
   )
   
-  scatterPlot_server(id = "module_1", mtcars)
-  scatterPlot_server(id = "module_2", iris)
-  scatterPlot_server(id = "module_3", ToothGrowth)
-  scatterPlot_server(id = "module_4", PlantGrowth)
-  scatterPlot_server(id = "module_5", USArrests)
+  scatterPlot_server(id = "tab1", mtcars)
+  scatterPlot_server(id = "tab2", iris)
+  scatterPlot_server(id = "tab3", ToothGrowth)
+  scatterPlot_server(id = "tab4", PlantGrowth)
+  scatterPlot_server(id = "tab5", USArrests)
+  
+  df_col1 = c(1)
+  df_col2 = c(2)
+  df = data.frame(df_col1, df_col2)
+  
+  QI_server(id = "tab1", df)
+  QI_server(id = "tab2", df)
+  QI_server(id = "tab3", df)
+  QI_server(id = "tab4", df)
+  QI_server(id = "tab5", df)
+  
 }
 
 shinyApp(ui = ui, server = server)
