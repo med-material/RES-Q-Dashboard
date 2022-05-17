504c53c8d024848de30ea08dc661f5b37a166c0f
gabCodes <47746090+gabCodes@users.noreply.github.com>
2022-05-17 16:16:27 +0200

    Moved the specific layout of each section of the app into their respective application

--- modules/tabPages_Layout.R
+++ modules/tabPages_Layout.R
@@ -1,31 +1,8 @@
 tabPages <- function(tab_id, tab_df) {
   fluidPage(
     fluidRow(
-      column(
-        5,
-        h1("Quality Indicators"),
-        tags$hr(),
-        tags$p("First Item in list"),
-        tags$hr(),
-        tags$p("Second Item"),
-        tags$hr(),
-        tags$p("Third Item"),
-        tags$hr(),
-        tags$p("Fourth Item"),
-        tags$hr(),
-        tags$p("Fifth Item"),
-        tags$hr(),
-        tags$p("Sixth Item")
-      ),
-      column(
-        7,
-        h1("Visualization"),
-        tabsetPanel(type = "tabs",
-                    tabPanel("Plot", scatterPlot_UI(id = tab_id, database = tab_df)),
-                    tabPanel("Table", scatterPlot_Table(id = tab_id))
-        )
-      )
+        QI_Layout(id = tab_id, database = tab_df),
+        visualization_Layout(id = tab_id, database = tab_df)
     )
   )
 }
-
