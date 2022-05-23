d77bf82554004485fa7a8ec47f4976cf5e3f1ddf
gabCodes <47746090+gabCodes@users.noreply.github.com>
2022-05-23 08:52:23 +0200

    Changing FluidRow to FixedRow

--- modules/tabPages_Layout.R
+++ modules/tabPages_Layout.R
@@ -1,8 +1,9 @@
 tabPages <- function(tab_id, tab_df) {
   fluidPage(
-    fluidRow(
-        QI_Layout(id = tab_id, database = tab_df),
-        visualization_Layout(id = tab_id, database = tab_df)
+    fixedRow(
+      QI_Layout(id = tab_id, database = tab_df),
+      visualization_Layout(id = tab_id, database = tab_df)
+
     )
   )
 }
