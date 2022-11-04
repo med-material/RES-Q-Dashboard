3f290d8386ef3e92561c519873cecd294fc7dafd
hendrikknoche <hk@create.aau.dk>
2022-11-04 10:25:57 +0100

    moved library calls for easier work

--- utils/DataAggregation.R
+++ utils/DataAggregation.R
@@ -1,10 +1,35 @@
 # import and data column definitions --------------------------------------
-source("utils/data_structures.R")
 library(tidyverse)
-library(ggplot2)
+source("utils/data_structures.R")
 dataset <- tibble::as_tibble(read.csv("data/dataREanonymized.csv"))
 #convert all empty strings to NA
 dataset[dataset == ""] <- NA
+#convert all columns based on boolean/logical values from char to logical
+to.conv <- sapply(dataset, function(x) all(x %in% c("False", "True", NA)))
+dataset[,to.conv] <- sapply(dataset[,to.conv], as.logical)
+
+#computations need to be verified with ICRC
+
+dataset <- dataset %>% 
+  mutate(
+    dnt_leq_60 = ifelse(stroke_type=='ischemic' & thrombolysis == T & hospital_stroke != T & first_hospital == T,ifelse(door_to_needle>60,0,1) ,NA),
+    dnt_leq_45 = ifelse(stroke_type=='ischemic' & thrombolysis == T & hospital_stroke != T & first_hospital == T,ifelse(door_to_needle>45,0,1) ,NA),
+    dgt_leq_120= ifelse(stroke_type=='ischemic' & thrombectomy == T & hospital_stroke != T & first_hospital == T,ifelse(door_to_groin>120,0,1), NA),
+    dgt_leq_90 = ifelse(stroke_type=='ischemic' & thrombectomy == T & hospital_stroke != T & first_hospital == T,ifelse(door_to_groin> 90,0,1) ,NA),
+    rec_total_is=ifelse(stroke_type=='ischemic', ifelse(thrombolysis== T,1,0) ,NA),
+    mri_first_hosp= ifelse(first_hospital==T, ifelse(imaging_done==T,1,0),NA),
+    isp_dis_antiplat = ifelse(discharge_destination!='dead',ifelse(discharge_any_antiplatelet==T,1,0),NA),
+    af_p_dis_anticoag = ifelse(discharge_destination!='dead',ifelse(discharge_any_anticoagulant==T,1,0),NA),
+    sp_hosp_stroke_unit_ICU = ifelse(hospitalized_in=='ICU/stroke unit',1,0)
+    # dysphagia_screening=ifelse(post_acute_care == 'yes' & stroke_type %in% c('ischemic','transient ischemic','intracerebral hemorrhage', 'undetermined'),NA)
+    )
+
+
+
+# "","p_dys_screen", "%in% c('GUSS','water test','other', 'ASSIST')" , 80, 85, 90, "post_acute_care == 'yes' & stroke_type %in% c('ischemic','transient ischemic','intracerebral hemorrhage', 'undetermined')",
+
+
+
 #sf
 #set fantasy hospital names
 hospital_Names <- c("Progress Center", "Paradise Clinic", "Angelvale Clinic", "Memorial Hospital", "Rose Clinic", "General Hospital", "Mercy Center", "Hope Hospital", "Samaritan Hospital")
@@ -25,28 +50,30 @@ dataset <- dataset %>%
   filter(discharge_year > 2000 & discharge_year <= as.integer(format(Sys.Date(), "%Y")))
 
 key_cols <- c(key_cols, "patient_id", "hospital", "hospital_name",
-              "hospital_country", "year", "quarter")
+              "hospital_country", "year", "quarter","YQ")
 
 # Relevant columns from the dataset for the QI's HARDCODED. There might be a smarter way to do this.
 
-numVars_cols <-c(key_cols,numVars)
+numVars_cols <-c(key_cols,numVars,pctCols)
 catVars_cols <-c(key_cols,catVars)
 
 # aggregation -------------------------------------------------------------
 agg_data <- dataset[, numVars_cols] %>%
   pivot_longer(-key_cols, names_to = "QI", values_to = "Value") %>%
   group_by(QI, hospital_country, hospital_name, year, quarter, YQ) %>%
-  summarise(median = median(Value, na.rm = TRUE)) 
-# %>%
-#   pivot_longer(cols = median, names_to = "agg_function", values_to = "Value")
+  summarise(median = median(Value, na.rm = TRUE),
+            pct = mean(Value, na.rm = TRUE)*100,
+            coverage_pct=sum(!(is.na(Value)))/n()) %>% 
+  pivot_longer(cols = median:coverage_pct, names_to = "agg_function", values_to = "Value")
 
 agg_data <- dataset[, numVars_cols] %>%
   pivot_longer(-key_cols, names_to = "QI", values_to = "Value") %>%
-  group_by(QI, hospital_country, hospital_name, year) %>%
+  group_by(QI, hospital_country, hospital_name, year, YQ) %>%
   summarise(
-    median = median(Value, na.rm = TRUE)
-  ) %>%
-  # pivot_longer(cols = median, names_to = "agg_function", values_to = "Value") %>%
+    median = median(Value, na.rm = TRUE),
+    pct = mean(Value, na.rm = TRUE)*100,
+    coverage_pct=sum(!(is.na(Value)))/n()) %>%
+  pivot_longer(cols = median:coverage_pct, names_to = "agg_function", values_to = "Value") %>%
   mutate(quarter = "all") %>%
   rbind(agg_data)
 
@@ -54,9 +81,10 @@ agg_data <- dataset[, numVars_cols] %>%
   pivot_longer(-key_cols, names_to = "QI", values_to = "Value") %>%
   group_by(QI, hospital_country, year, quarter) %>%
   summarise(
-    median = median(Value, na.rm = TRUE)
-  ) %>%
-  # pivot_longer(cols = median, names_to = "agg_function", values_to = "Value") %>%
+    median = median(Value, na.rm = TRUE),
+    pct = mean(Value, na.rm = TRUE)*100,
+    coverage_pct=sum(!(is.na(Value)))/n()) %>%
+  pivot_longer(cols = median:coverage_pct, names_to = "agg_function", values_to = "Value") %>%
   mutate(hospital_name = "all") %>%
   rbind(agg_data)
 
@@ -64,9 +92,10 @@ agg_data <- dataset[, numVars_cols] %>%
   pivot_longer(-key_cols, names_to = "QI", values_to = "Value") %>%
   group_by(QI, hospital_country, year) %>%
   summarise(
-    median = median(Value, na.rm = TRUE)
-  ) %>%
-  # pivot_longer(cols = median, names_to = "agg_function", values_to = "Value") %>%
+    median = median(Value, na.rm = TRUE),
+    pct = mean(Value, na.rm = TRUE)*100,
+    coverage_pct=sum(!(is.na(Value)))/n()) %>%
+  pivot_longer(cols = median:coverage_pct, names_to = "agg_function", values_to = "Value") %>%
   mutate(quarter = "all",hospital_name = "all") %>%
   rbind(agg_data) %>%
   mutate(subgroup=NA)
@@ -115,7 +144,15 @@ agg_data <- dataset[, catVars_cols] %>%
   mutate(hospital_name = "all") %>%
   rbind(agg_data)
 
-agg_data <- agg_data 
+#REMOVE means for standard numerica vars and medians for the percentages
+agg_data <- agg_data %>%
+  filter((QI %in% pctCols & agg_function=='pct') | 
+           (QI %in% numVars  & (agg_function=='median'|agg_function=='median' ))| 
+           (QI %in% catVars))
+
+# agg_data <- 
+#   agg_data %>% 
+#   filter
 
 
 
@@ -123,14 +160,13 @@ agg_data <- agg_data
 # AngelThresholds - define dnt_leq_60 etc. 
 # first time above (angel/national/own) threshold 
 # last4Quarter values instead of year/quarter, 
-#dataset %>% .[.$YQ=="2022 Q1",]
+# dataset %>% .[.$YQ=="2022 Q1",]   SUM_IF
 
 options("scipen"=999)
 
 # show only one specific hospital - reducing the rows in the dataset for debugging. 
 # agg_data<-agg_data %>% filter(hospital_name == "Progress Center")
 
-aa_cols<-c(angel_awards$QI,"stroke_type","first_hospital","hospital_stroke","thrombectomy","post_acute_care")
 
 cond_dataset<-dataset%>% 
   select(unique(aa_cols)) %>%
