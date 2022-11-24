416554b85765b9f0f089e218fc6640e3344f9844
mathiassand <mathiassand02@gmail.com>
2022-11-24 12:18:55 +0100

    push latest

--- utils/DataAggregation.R
+++ utils/DataAggregation.R
@@ -369,14 +369,16 @@ dfx <- agg_dataNum
 
 dfx<-agg_dataNum%>%
   filter(isYearAgg == FALSE, isAngelKPI == TRUE,
-         h_name == "General", nameOfAggr == "dnt_leq_45", 
-         is.na(subGroup), !is.na(YQ)) %>%
+         h_name == "General", nameOfAggr == "dnt_leq_60", 
+         is.na(subGroup), !is.na(YQ), year == "2019") %>%
   mutate(condColor = ifelse(is1stDiam>0, "#7cd461", "#4299f5")) %>%
   ggplot(aes(x = YQ, y = Value, group = 1)) + 
   geom_line(color="#42aaf5") +
   geom_line(aes(y=C_Value),color="grey") + 
   geom_point(aes(size=data_Pts,color=condColor)) + 
-  scale_color_identity()
+  scale_color_identity() + 
+  theme_minimal() + 
+  ylab("dnt less or equal to 60, in %.")
 
 dfx
     
